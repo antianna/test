1a8d5757edd07dc2c7bd35facbb4e2871a4c71a7
Anna Antoniou <anna.antoniou@gmail.com>
2019-03-09 19:30:09 +0000

    Initialising folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I am testing the template',
+    author='anna',
+    license='MIT',
+)
